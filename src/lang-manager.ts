99ea0edf4f05528a31f060a109d51c76eabb3f1a
krypek <krypek@tuta.io>
2024-02-29 18:13:43 +0100

    Remove console.log

--- src/lang-manager.ts
+++ src/lang-manager.ts
@@ -16,7 +16,6 @@ export class LangManager {
     }
 
     async poststart() {
-        console.log(ig.lang)
         ig.lang.labels.sc.gui.menu['menu-titles'].mods = Lang.mods
         ig.merge(ig.lang.labels.sc.gui.menu.sort, Lang.sort)
         ig.lang.labels.sc.gui.menu['help-texts'].mods = Lang.help
