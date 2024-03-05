1e06bf7b0e01a006f75145c46ecf9080dc5621e6
krypek <krypek@tuta.io>
2024-03-05 16:46:54 +0100

    Improve pack.sh

--- pack.sh
+++ pack.sh
@@ -2,10 +2,18 @@
 BASE_NAME="$(jq '.id' ccmod.json | sed 's/^"//;s/"$//')"
 NAME="${BASE_NAME}-$(jq '.version' ccmod.json | sed 's/^"//;s/"$//').ccmod"
 rm -rf "$BASE_NAME"*
-echo not running in build mode
 npm install
-# npm run build
-npm run start
-zip -r "$NAME" ./ -x "*.ccmod" "*.zip" "node_modules/*" ".git*" "*.ts" "*.md" \
-    "tsconfig.json" "*.sh" "package-lock.json" "pack/*" "bundle.zip" ".prettierrc.json" \
-    "*.kra" "*.kra~" "*.png~" "src/*"
+npm run build
+mkdir -p pack
+cp -r assets lang icon LICENSE plugin.js ./pack
+cd ./pack
+for file in $(find . -iname '*.json') $(find . -iname '*.json.patch') $(find . -iname '*.json.patch.cond'); do
+    jq '.' ../$file -c > $file
+done
+cp ../ccmod.json .
+rm -rf icon/icon240.png **.kra *.png~
+LIST="$(find -name '*.kra') $(find -name '*.png~')"
+rm -rf $LIST
+zip -r "../$NAME" .
+cd ..
+rm -rf pack
