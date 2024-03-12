c66d43c35ff91ad2d9c2e3a8073aa2416ecd2770
krypek <krypek@tuta.io>
2024-03-12 12:45:51 +0100

    Fix workflows and pack.sh

--- pack.sh
+++ pack.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
+set -e
 BASE_NAME="$(jq '.id' ccmod.json | sed 's/^"//;s/"$//')"
 NAME="${BASE_NAME}-$(jq '.version' ccmod.json | sed 's/^"//;s/"$//').ccmod"
 rm -rf "$BASE_NAME"*
 pnpm install
 pnpm run build
 mkdir -p pack
-cp -r assets lang icon LICENSE plugin.js ./pack
+cp -rf icon LICENSE plugin.js ./pack
+[ -d ./assets ] && cp -r assets ./pack
+[ -d ./lang ] && cp -r lang ./pack
+
 cd ./pack
 for file in $(find . -iname '*.json') $(find . -iname '*.json.patch') $(find . -iname '*.json.patch.cond'); do
     jq '.' ../$file -c > $file
 done
 cp ../ccmod.json .
-rm -rf icon/icon240.png **.kra *.png~
-LIST="$(find -name '*.kra') $(find -name '*.png~')"
+rm -rf icon/icon240.png
+LIST="$(find . -name '*.kra') $(find . -name '*.png~')"
 rm -rf $LIST
 zip -r "../$NAME" .
 cd ..
