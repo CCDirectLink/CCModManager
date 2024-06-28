a513638a15ab0c187352d6701b36a18874b820a2
krypek <krypek@tuta.io>
2024-06-28 14:56:36 +0200

    Add documentation to src/mod-options.ts

--- src/mod-options.ts
+++ src/mod-options.ts
@@ -1,11 +1,13 @@
 export type Enum = Record<string, number>
 
+/** Option that has state that can change */
 export interface OptionChangeable {
     restart?: boolean
     changeEvent?: () => void
     updateMenuOnChange?: boolean
 }
 
+/** A option entry */
 // prettier-ignore
 export type Option = {
     name?: ig.LangLabel.Data
@@ -214,9 +216,12 @@ const defaultLanguageGetter: ModOptionsSettings.LanguageGetter = (category, head
 
 declare global {
     namespace modmanager {
+        /** Contains the layouts used for displaying the mod options menu */
         var optionConfigs: Record<string, ModSettingsGui>
+        /** Contains the options objects for each registered mod. */
         var options: Record<string, Record<string, any>>
 
+        /** Register the mod options. Don't run it in preload to avoid race conditions, prestart and above is fine. */
         function registerAndGetModOptions<T extends Options>(settings: ModOptionsSettings, options: T): OptsType<T>
     }
 }
