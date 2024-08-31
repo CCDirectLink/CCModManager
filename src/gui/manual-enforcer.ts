6fbb825daa2426f0bfca91dfaae503ebcca4a2cc
krypek <krypek@tuta.io>
2024-08-31 12:34:35 +0200

    Make the manual enforcer less anoyinng

--- src/gui/manual-enforcer.ts
+++ src/gui/manual-enforcer.ts
@@ -26,7 +26,6 @@ modmanager.gui.ManualEnforcer = modmanager.gui.MultiPageButtonBoxGui.extend({
     init(id, manualTitle, manualContent, forceShowEvenIfRead, enforceTimeSeconds = 5) {
         if (Opts.manualEnforcerRead[id] && !forceShowEvenIfRead) return
 
-        let failuteToReadCount = 0
         this.parent(undefined, undefined, [
             {
                 name: Lang.help.enforcment.button,
@@ -40,20 +39,12 @@ modmanager.gui.ManualEnforcer = modmanager.gui.MultiPageButtonBoxGui.extend({
                         }
                     } else {
                         const button = this.userButtons![0]
-                        // prettier-ignore
-                        const text =
-                            failuteToReadCount == 0 ? Lang.help.enforcment.fail0
-                          : failuteToReadCount == 1 ? Lang.help.enforcment.fail1
-                          : failuteToReadCount == 2 ? Lang.help.enforcment.fail2
-                          : failuteToReadCount == 3 ? Lang.help.enforcment.fail3
-                          : Lang.help.enforcment.fail4
+                        const text = Lang.help.enforcment.fail
                         sc.Dialogs.showErrorDialog(text, false, () => {
-                            if (failuteToReadCount == 4) throw new Error('MANUAL_NOT_READ')
                             button.setActive(false)
                             this.openendAt = Date.now()
-                            failuteToReadCount++
 
-                            setTimeout(() => button.setActive(true), (enforceTimeSeconds / 2) * 1000)
+                            setTimeout(() => button.setActive(true), enforceTimeSeconds * 1000)
                         })
                     }
                 },
