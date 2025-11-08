ed32dadec44a6bb4ed708671976777416f149438
krypek <krypek@tuta.io>
2025-11-08 15:54:51 +0100

    Remove old src/types

--- types/add-prototypes.d.ts
+++
@@ -1,9 +0,0 @@
-export {};
-declare global {
-    interface Object {
-        keysT<K extends string | number | symbol, V>(object: Record<K, V>): K[];
-        entriesT<K extends string | number | symbol, V>(object: {
-            [key in K]?: V;
-        }): [K, V][];
-    }
-}
